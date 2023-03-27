a68edd8da4416a660796d8da3c4c6e1c85180128
unknown <mary.liss133@gmail.com>
2023-03-27 17:03:03 +0500

    Внедрит репозиторий в качестве зависимости

--- project/apps/users/src/app/authentication/authentication.service.ts
+++ project/apps/users/src/app/authentication/authentication.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { WorkplaceUserMemoryRepository } from '../workplace-user/workplace-user-memory.repository.js';
 
 @Injectable()
-export class AuthenticationService {}
+export class AuthenticationService {
+    constructor(
+      private readonly blogUserRepository: WorkplaceUserMemoryRepository
+    ) {}
+  
+  }
