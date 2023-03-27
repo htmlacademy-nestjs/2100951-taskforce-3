7a28ac89bb3f5cd0da859cc1851f335c63187638
unknown <mary.liss133@gmail.com>
2023-03-27 18:15:06 +0500

    Добавит метод регистрации пользователя в сервис Authentication…Service

--- project/apps/users/src/app/authentication/authentication.service.ts
+++ project/apps/users/src/app/authentication/authentication.service.ts
@@ -1,10 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import dayjs from 'dayjs';
 import { WorkplaceUserMemoryRepository } from '../workplace-user/workplace-user-memory.repository.js';
+import { WorkplaceUserEntity } from '../workplace-user/workplace-user.entity.js';
+import { AUTH_USER_EXISTS } from './authentication.constant.js';
+import { CreateUserDto } from './dto/create-user.dto.js';
 
 @Injectable()
 export class AuthenticationService {
     constructor(
-      private readonly blogUserRepository: WorkplaceUserMemoryRepository
+      private readonly workplaceUserRepository: WorkplaceUserMemoryRepository
     ) {}
   
+    public async register(dto: CreateUserDto) {
+        const {email, firstname, lastname, password, dateBirth, city, role} = dto;
+    
+        const workplaceUser = {
+          email, firstname, lastname, role, city,
+          avatar: '', dateBirth: dayjs(dateBirth).toDate(),
+          passwordHash: ''
+        };
+    
+        const existUser = await this.workplaceUserRepository
+          .findByEmail(email);
+    
+        if (existUser) {
+          throw new ConflictException(AUTH_USER_EXISTS);
+        }
+    
+        const userEntity = await new WorkplaceUserEntity(workplaceUser)
+          .setPassword(password)
+    
+        return this.workplaceUserRepository
+          .create(userEntity);
+      }
+    
   }
