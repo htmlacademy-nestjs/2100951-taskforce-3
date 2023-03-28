5c3f898ea82ca4fb05bf32e086cb0116a41abc7d
unknown <mary.liss133@gmail.com>
2023-03-28 19:14:31 +0500

    Добавит описание для LoginUserDto (@ApiProperty)

--- project/apps/users/src/app/authentication/dto/create-user.dto.ts
+++ project/apps/users/src/app/authentication/dto/create-user.dto.ts
@@ -1,12 +1,48 @@
-import { UserRole } from "@project/shared/app-types";
+import { ApiProperty } from '@nestjs/swagger';
+import { UserRole } from '@project/shared/app-types';
 
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'User unique address',
+    example: 'user@user.ru'
+  })
   public email: string;
+
+  @ApiProperty({
+    description: 'User birth date',
+    example: '1981-03-12',
+
+  })
   public dateBirth: string;
+
+  @ApiProperty({
+    description: 'User first name',
+    example: 'Keks',
+  })
   public firstname: string;
+
+  @ApiProperty({
+    description: 'User last name',
+    example: 'Ivanov'
+  })
   public lastname: string;
+
+
+  @ApiProperty({
+    description: 'User password',
+    example: '123456'
+  })
   public password: string;
+
+  @ApiProperty({
+    description: 'User city',
+    example: 'Ufa'
+  })
   public city: string;
+
+  @ApiProperty({
+    description: 'User role',
+    example: 'executor'
+  })
   public role: UserRole;
-  public avatar?: string;
 }
