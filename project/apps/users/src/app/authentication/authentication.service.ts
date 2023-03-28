762449f09bc477197094d9b24abf7ff477ab27fd
unknown <mary.liss133@gmail.com>
2023-03-28 14:03:11 +0500

    Добавит метод для авторизации пользователя

--- project/apps/users/src/app/authentication/authentication.service.ts
+++ project/apps/users/src/app/authentication/authentication.service.ts
@@ -1,9 +1,10 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import dayjs from 'dayjs';
 import { WorkplaceUserMemoryRepository } from '../workplace-user/workplace-user-memory.repository.js';
 import { WorkplaceUserEntity } from '../workplace-user/workplace-user.entity.js';
-import { AUTH_USER_EXISTS } from './authentication.constant.js';
+import { AUTH_USER_EXISTS, AUTH_USER_NOT_FOUND, AUTH_USER_PASSWORD_WRONG } from './authentication.constant.js';
 import { CreateUserDto } from './dto/create-user.dto.js';
+import { LoginUserDto } from './dto/login-user.dto.js';
 
 @Injectable()
 export class AuthenticationService {
@@ -33,5 +34,24 @@ export class AuthenticationService {
         return this.workplaceUserRepository
           .create(userEntity);
       }
+
+      public async verifyUser(dto: LoginUserDto) {
+        const {email, password} = dto;
+        const existUser = await this.workplaceUserRepository.findByEmail(email);
     
+        if (!existUser) {
+          throw new NotFoundException(AUTH_USER_NOT_FOUND);
+        }
+    
+        const blogUserEntity = new WorkplaceUserEntity(existUser);
+        if (!await blogUserEntity.comparePassword(password)) {
+          throw new UnauthorizedException(AUTH_USER_PASSWORD_WRONG);
+        }
+    
+        return blogUserEntity.toObject();
+      }
+    
+      public async getUser(id: string) {
+        return this.workplaceUserRepository.findById(id);
+      }    
   }
