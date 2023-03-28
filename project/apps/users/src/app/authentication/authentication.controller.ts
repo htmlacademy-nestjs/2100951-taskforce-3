f56b07f72843bf9b2a7d2ba2e13bfebd42405cac
unknown <mary.liss133@gmail.com>
2023-03-28 18:47:59 +0500

    Добавит обработчик маршрута /register

--- project/apps/users/src/app/authentication/authentication.controller.ts
+++ project/apps/users/src/app/authentication/authentication.controller.ts
@@ -1,9 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { fillObject } from '@project/util/util-core';
 import { AuthenticationService } from './authentication.service.js';
+import { CreateUserDto } from './dto/create-user.dto.js';
+import { UserRdo } from './rdo/usre.rdo.js';
 
 @Controller('auth')
 export class AuthenticationController {
     constructor(
       private readonly authService: AuthenticationService
     ) {}
+
+    @Post('register')
+    public async create(@Body() dto: CreateUserDto) {
+      const newUser = await this.authService.register(dto);
+      return fillObject(UserRdo, newUser);
+    }
   }
