5be9b26b6dd4b1ac93d72b5665905ed0f45e4db4
unknown <mary.liss133@gmail.com>
2023-04-18 12:32:46 +0500

    Сохранение информации о загруженных файлах в бд

--- project/libs/shared/app-types/src/index.ts
+++ project/libs/shared/app-types/src/index.ts
@@ -7,3 +7,4 @@ export * from './lib/review.interface';
 export * from './lib/city.enum';
 export * from './lib/category.interface';
 export * from './lib/response.interface';
+export * from './lib/file.interface';
